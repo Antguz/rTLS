26d9c233fa87148b8c506d54ce4201bc8aa82059
Antguz <antguz06@gmail.com>
2018-12-23 09:46:12 -0700

    major changes to the documentation

--- R/neighborhood.R
+++ R/neighborhood.R
@@ -4,18 +4,27 @@
 #'
 #' @description Estimate neighboring points based on two methods.
 #'
-#' @param cloud A matrix or data.frame with xyz coordinates in the first three columns.
-#' @param method A character string specifying the method to estimated the neighbors. It most be one of "distance" or "knn".
-#' @param radius An integer of a length 1 representing the number of neighbors to consider. This will be used if method = "distance".
-#' @param k An integer of a length 1 representing the number of neighbors to consider. This will be used if method = "knn".
-#' @param parallel Logical, if TRUE it use a parallel processing.
+#' @param cloud A  \code{matrix} or  \code{data.frame} with xyz coordinates in the first three columns.
+#' @param method A character string specifying the method to estimated the neighbors. It most be one of  \code{"distance"} or  \code{"knn"}.
+#' @param radius An integer of a length 1 representing the number of neighbors to consider. This will be used if  \code{method = "distance"}.
+#' @param k An integer of a length 1 representing the number of neighbors to consider. This will be used if  \code{method = "knn"}.
+#' @param parallel Logical, if  \code{TRUE} it use a parallel processing.
 #'
 #' @value An object of class "neighborhood" with a list per point of the neighboring points.
 #' @author J. Antonio Guzman Q. and Ronny Hernandez
 #' @references Wang, D., Hollaus, M., & Pfeifer, N. (2017). Feasibility of machine learning methods for separating wood and leaf points from Terrestrial Laser Scanning data. ISPRS Annals of Photogrammetry, Remote Sensing & Spatial Information Sciences, 4.
 #'
+#' @examples
+#' data("pc_tree")
+#'
+#' ###Estimate the niegborhood based on distance
+#' neighborhood(pc_tree, method = "distance", radius = 0.2)
+#'
+#' ###Estimate the niegborhood based on knn
+#' neighborhood(pc_tree, method = "knn", k = 10)
+#'
 #'@export
-neighborhood <- function(cloud, method, radius, k, parallel, ...) {
+neighborhood <- function(cloud, method, radius, k, parallel) {
 
   if(parallel == TRUE) { ###Parallel TRUE
 
