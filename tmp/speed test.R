5c1654fe31e29787bddc5b28f2f25ff15b4158c9
Antguz <antguz06@gmail.com>
2019-01-21 20:12:57 -0700

    Implementing data.table on speed test

--- tmp/speed test.R
+++ tmp/speed test.R
@@ -10,7 +10,7 @@ cloud_b <- pc_tree
 radius <- 0.5
 parallel = FALSE
 
-system.time(results_1 <- alply(cloud, .margins = 1, .fun = sphere_neighbors, cloud = cloud, radius = radius, .progress = "text", .parallel = parallel, .paropts = pack, .inform = FALSE))
+system.time(results_1 <- alply(cloud, .margins = 1, .fun = sphere_neighbors, cloud = cloud_b, radius = radius, .progress = "text", .parallel = parallel, .paropts = pack, .inform = FALSE))
 
 
 ###With purrrlyr
@@ -33,3 +33,32 @@ cloud_b <- pc_tree
 radius <- 0.5
 
 system.time(results_3 <- cloud %>% rowwise %>% do(neighborhood = sphere_neighbors(data_frame(.$X, .$Y, .$Z) , cloud = cloud_b, radius = radius)))
+
+###With data.table
+#---------------------------------------#
+data(pc_tree)
+pc_tree <- as.data.table(pc_tree)
+cloud <- pc_tree[1:5000,]
+cloud_b <-  pc_tree
+radius <- 0.5
+
+sphere_neighbors2 <- function(x, cloud, radius) {
+
+  xcoor <- as.numeric(x[1,1])
+  ycoor <- as.numeric(x[1,2])
+  zcoor <- as.numeric(x[1,3])
+
+  cube <- cloud[between(X, xcoor - radius, xcoor + radius) & between(Y, ycoor - radius, ycoor + radius) & between(Z, zcoor - radius, zcoor + radius),]
+
+  cube <- cube[,1:3]
+
+  cube <- cube[,distance := sqrt((xcoor - cube$X)^2 + (ycoor - cube$Y)^2 + (zcoor - cube$Z)^2)]
+
+  space <- cube[cube$distance <= radius & cube$distance > 0,]
+  space <- space[order(distance),]
+  return(space)
+}
+
+system.time(results_4 <- cloud[, sphere_neighbors2(.SD, cloud_b, radius), by = seq_len(nrow(cloud))])
+
+
