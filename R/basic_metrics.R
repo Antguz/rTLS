bfe89814e9792bfa30be9863c08f6cebea5f8305
Antguz <antguz06@gmail.com>
2019-01-23 17:06:19 -0700

    implementing basic_metrics with data.table

--- R/basic_metrics.R
+++ R/basic_metrics.R
@@ -1,5 +1,3 @@
-#' @import dplyr
-#'
 #' @title Basic metrics of the neighboring points.
 #'
 #' @description Estimate basic metrics of the surrunding neigboring points created based on a target point.
@@ -15,23 +13,22 @@
 #'
 #' neig <- neighborhood(pc_tree[50,], pc_tree, method = "sphere", radius = 0.2)
 #'
-#' basic.metrics(neig$neighborhood$`1`, radius = neig$parameter)
+#' basic_metrics(neig$neighborhood[, 2:5], radius = neig$parameter)
 #'
 #' @export
 basic_metrics <- function(space, radius = NULL) {
 
-  if(length(space[,1]) >= 1) {
+  if(nrow(space) >= 1) {
     radius <- ifelse(is.null(radius) == TRUE, max(space[,4]), radius)
-
-    n_neig <- length(space[,4])
-    neig_volumen <- ((4/3)*pi*(max(space[,4])^3))
+    n_neig <- nrow(space)
+    neig_volumen <- ((4/3)*pi*(max(space$distance)^3))
     sphere_volumen <- ((4/3)*pi*(radius^3))
     density <- n_neig/sphere_volumen
 
-    frame <- data.frame(n_neig, neig_volumen, sphere_volumen, density)
+    frame <- data.table(n_neig, neig_volumen, sphere_volumen, density)
 
-  } else if(length(space[,1]) < 1) {
-    frame <- data.frame(n_neig = NA, neig_volumen = NA, sphere_volumen = NA, density = NA)
+  } else if(nrow(space) < 1) {
+    frame <- data.table(n_neig = NA, neig_volumen = NA, sphere_volumen = NA, density = NA)
 
   }
   return(frame)
