e34479e2dd0f0c15859eeaa70e5d286a1a8e9943
Antguz <antguz06@gmail.com>
2019-07-10 15:47:26 -0600

    Updating voxel_counting

--- R/voxels_counting.R
+++ R/voxels_counting.R
@@ -3,7 +3,8 @@
 #' @description Creates voxels of different size on a point cloud using the \code{\link{voxels}} function, and then return a \code{\link{summary.voxels}} of their features.
 #'
 #' @param cloud A \code{data.table} with xyz coordinates of the point clouds in the first three columns.
-#' @param voxel.range A positive \code{numeric} vector describing the different voxel size to perform. If \code{NULL}, it use 10 voxel sizes by defaul based on the largest range of XYZ. See details.
+#' @param voxel.sizes A positive \code{numeric} vector describing the different voxel size to perform. If \code{NULL}, it use 10 voxel sizes by defaul based on the largest range of XYZ. See details.
+#' @param min.sizes A positive \code{numeric} vector of length 1 describing the minimum voxel size to perform. This is required if \code{voxel.sizes = NULL}.
 #' @param bootstrap Logical. If \code{TRUE}, it computes a bootstrap on the H index calculations. \code{FALSE} as default.
 #' @param R A positive \code{integer} of length 1 indicating the number of bootstrap replicates. This need to be used if \code{bootstrap = TRUE}.
 #' @param parallel Logical, if \code{TRUE} it uses a parallel processing for the voxelization. \code{FALSE} as default.
@@ -25,25 +26,38 @@
 #' data(pc_tree)
 #'
 #' #Applying voxels counting
-#' voxels_counting(pc_tree)
+#' voxels_counting(pc_tree, min.size = 0.05)
 #'
 #' #Voxels counting using boostrap on the H indixes with 1000 repetitions
-#' voxels_counting(pc_tree, bootstrap = TRUE, R = 1000)
+#' voxels_counting(pc_tree, min.size = 0.05, bootstrap = TRUE, R = 1000)
 #'
 #' \dontrun{
 #' #Voxels counting using bootstrap on the H indices with 1000 repetitions and applying parallel processing with 4 cores.
-#' voxels_counting(pc_tree, bootstrap = TRUE, R = 1000, parallel = TRUE, cores = 4)
+#' voxels_counting(pc_tree, min.size = 0.05, bootstrap = TRUE, R = 1000, parallel = TRUE, cores = 4)
 #' }
 #'
 #' @export
-voxels_counting <- function(cloud, voxel.range = NULL, bootstrap = FALSE, R = NULL, parallel = FALSE, cores = NULL) {
+voxels_counting <- function(cloud, voxel.sizes = NULL, min.size, bootstrap = FALSE, R = NULL, parallel = FALSE, cores = NULL) {
 
   colnames(cloud) <- c("X", "Y", "Z")
 
-  if(is.null(voxel.range) == TRUE) { ###Default voxel.range
+  if(is.null(voxel.sizes) == TRUE) { ###Default voxel.sizes
     ranges <- c(max(cloud[,1]) - min(cloud[,1]), max(cloud[,2]) - min(cloud[,2]), max(cloud[,3]) - min(cloud[,3]))
     max.range <- ranges[which.max(ranges)] + 0.001
-    voxel.range <- c(max.range, max.range/2, max.range/4, max.range/8, max.range/16, max.range/32, max.range/64, max.range/128, max.range/256, max.range/512, max.range/1024)
+    voxel.sizes <- c(max.range)
+
+    n <- 2
+
+    repeat{
+      new.range <- voxel.sizes[1]/n
+
+      if(new.range >= min.size) {
+        voxel.sizes <- c(voxel.sizes, new.range)
+        n <- n * 2
+      } else {
+        break
+      }
+    }
   }
 
   cloud_touse <- cloud
@@ -55,14 +69,14 @@ voxels_counting <- function(cloud, voxel.range = NULL, bootstrap = FALSE, R = NU
     registerDoSNOW(cl)
 
     print("Creating voxels in parallel")  #Progress bar
-    pb <- txtProgressBar(min = 0, max = length(voxel.range), style = 3)
+    pb <- txtProgressBar(min = 0, max = length(voxel.sizes), style = 3)
     progress <- function(n) setTxtProgressBar(pb, n)
     opts <- list(progress=progress)
 
     #Run in parallel
-    results <- foreach(i = 1:length(voxel.range), .inorder = FALSE, .combine= rbind, .packages = c("data.table", "rTLS"), .options.snow = opts) %dopar% {
-      vox <- voxels(cloud_touse, voxel.size = voxel.range[i], obj.voxels = FALSE)
-      summary <- summary.voxels(vox, voxel.size = voxel.range[i], bootstrap = bootstrap, R = R)
+    results <- foreach(i = 1:length(voxel.sizes), .inorder = FALSE, .combine= rbind, .packages = c("data.table", "rTLS"), .options.snow = opts) %dopar% {
+      vox <- voxels(cloud_touse, voxel.size = voxel.sizes[i], obj.voxels = FALSE)
+      summary <- summary.voxels(vox, voxel.size = voxel.sizes[i], bootstrap = bootstrap, R = R)
       return(summary)
     }
 
@@ -73,13 +87,13 @@ voxels_counting <- function(cloud, voxel.range = NULL, bootstrap = FALSE, R = NU
 
   } else if(parallel == FALSE) { ###If parallel is false
 
-    pb <- txtProgressBar(min = 0, max = length(voxel.range), style = 3) #Progress bar
+    pb <- txtProgressBar(min = 0, max = length(voxel.sizes), style = 3) #Progress bar
 
     #Run without using parallel
-    results <- foreach(i = 1:length(voxel.range), .inorder = FALSE, .combine= rbind) %do% {
+    results <- foreach(i = 1:length(voxel.sizes), .inorder = FALSE, .combine= rbind) %do% {
       setTxtProgressBar(pb, i)
-      vox <- voxels(cloud_touse, voxel.size = voxel.range[i], obj.voxels = FALSE)
-      summary <- summary.voxels(vox, voxel.size = voxel.range[i], bootstrap = bootstrap, R = R)
+      vox <- voxels(cloud_touse, voxel.size = voxel.sizes[i], obj.voxels = FALSE)
+      summary <- summary.voxels(vox, voxel.size = voxel.sizes[i], bootstrap = bootstrap, R = R)
       return(summary)
     }
     results <- results[order(Voxel.size)]
