9aad48eae1c553b12dacc084f73acae131e2a0ef
Antguz <antguz06@gmail.com>
2019-07-16 19:50:33 -0600

    changing canopy_structue

--- R/canopy_structure.R
+++ R/canopy_structure.R
@@ -1,8 +1,9 @@
-#' @title Forest Structure
+#' @title Canopy Structure
 #'
-#' @description Estimates the forest structure of discrete returns from a TLS scan.
+#' @description Estimates the canopy structure of discrete returns from a TLS scan.
 #'
 #' @param scan A \code{data.table} with three columns with describe *XYZ* coordinates of the discrete return of a TLS scan.
+#' @param returns A \code{character} describing is the TLS used is \code{"single"} or \code{"multiple"} return. \code{"single"} as default.
 #' @param zenith.range A \code{numeric} vector of length two describing the \code{min} and \code{max} range of the zenith angle to use. Theoretically, the \code{max} range should be lower than 90 degrees.
 #' @param zenith.bands A \code{numeric} vector of length one describing the number of bands to use between the \code{zenith.range} to estimate the frecuency of laser shots and measured points in \code{scan}.
 #' @param azimuth.range A \code{numeric} vector of length two describing the range of the azimuth angle to use. Theoretically, it should be between 0 and 360 degrees.
@@ -38,23 +39,28 @@
 #' @examples
 #' scan <- fread("C:/Users/josea/Documents/Github/tmp/Example.txt", sep = "\t")
 #' scan <- scan[, 1:3]
-#' scan[, c("X", "Y", "Z") := round(.SD, 5), .SDcols= c("X", "Y", "Z")]
+#' scan[, c("X", "Y", "Z") := round(.SD, 3), .SDcols= c("X", "Y", "Z")]
+#' returns <- "multiple"
 #' zenith.range <- c(50.0000, 70.0000)
-#' zenith.bands <- 6
+#' zenith.bands <- 4
 #' azimuth.range <- c(0.0000, 360.0000)
 #' TLS.resolution <- c(0.04, 0.04)
 #' TLS.coordinates <- c(0, 0, 0)
-#' TLS.frame <- c(30.0000, 150.0000, 0, 360.0000)
-#' TLS.angles <- c(-1.135, 2.434, 147.247)
+#' TLS.frame <- c(30.0000, 130, 0, 360.0000)
+#' TLS.angles <- c(0.293, -0.835, -150.159)
 #' parallel <- TRUE
 #' cores <- 4
 #'
-#' a <- fstructure(scan, zenith.range, zenith.bands, azimuth.range, TLS.resolution, TLS.coordinates, TLS.frame, TLS.angles, parallel, cores )
+#' a <- fstructure(scan, returns = "single", zenith.range, zenith.bands, azimuth.range, TLS.resolution, TLS.coordinates, TLS.frame, TLS.angles, parallel, cores )
 #'
 #' @export
-fstructure <- function(scan, zenith.range, zenith.bands, azimuth.range, TLS.resolution, TLS.coordinates = NULL, TLS.frame = NULL, TLS.angles = NULL, parallel = FALSE, cores = NULL) {
+canopy_structure <- function(scan, returns = "single", zenith.range, zenith.bands, azimuth.range, TLS.resolution, TLS.coordinates = NULL, TLS.frame = NULL, TLS.angles = NULL, parallel = FALSE, cores = NULL) {
 
-  colnames(scan)[1:3] <- c("X", "Y", "Z")
+  if(returns == "multiple") {
+    colnames(scan)[1:4] <- c("X", "Y", "Z", "Target_count")
+  } else if(returns == "single") {
+    colnames(scan)[1:3] <- c("X", "Y", "Z")
+  }
 
   ###Validate assumptions-------------------------------------------------------------------------------------------------------
   if(parallel == TRUE & is.null(cores) == TRUE) {
@@ -81,67 +87,56 @@ fstructure <- function(scan, zenith.range, zenith.bands, azimuth.range, TLS.reso
 
   ###Estimates reaturns angles based on the TLS coordinates--------------------------------------------------------------------------------------
 
-  scan <- cbind(scan, cartesian_to_polar(scan[, 1:3], TLS.coordinates, 3)[,1:2])
+  scan <- cbind(scan, cartesian_to_polar(scan[, 1:3], TLS.coordinates, 2)[,1:2])
 
   if(returns == "multiple") {
-    pulses <- scan[, .(w = 1/.N), by = c("zenith", "azimuth")]
-    scan <- merge(scan, pulses, by = c("zenith", "azimuth"))
-    scan <- scan[, c(3,4,5,1,2,6)]
+    scan[, w := round(1/Target_count, 3),]
+  } else if(returns == "single") {
+    scan$w <- 1
   }
 
   scan <- scan[between(zenith, zenith.range[1], zenith.range[2]),]
-  scan[, h := Z - TLS.coordinates[3]]
-  scan <- scan[h >= 0,]
 
   ###Estimate the number of scanner pulses in a given zenith and azimuth range --------------------------------------------------------------
 
   scanner <- CJ(zenith = seq(TLS.frame[1], TLS.frame[2], TLS.resolution[1]),
-              azimuth = seq(TLS.frame[3], TLS.frame[4], TLS.resolution[2]))
+              azimuth = seq(TLS.frame[3], TLS.frame[4], TLS.resolution[2])) #Create grid
   scanner$distance <- 1.000
-  scanner <- polar_to_cartesian(scanner, digits = 5)
-
-  scanner <- move_rotate(scanner, move = NULL, rotate = c(TLS.angles[1], TLS.angles[2], TLS.angles[3]))
-  scanner <- cartesian_to_polar(scanner, NULL, digits = 3)
-
-  scanner <- scanner[between(zenith, zenith.range[1], zenith.range[2]) , 1:2]
+  scanner <- polar_to_cartesian(scanner, digits = 3) #Get cartesian
+  scanner <- move_rotate(scanner, move = NULL, rotate = c(TLS.angles[1], TLS.angles[2], TLS.angles[3])) #Correction of angles
+  scanner <- scanner[Z >= 0] #Subset of values
+  scanner <- cartesian_to_polar(scanner, NULL, digits = 2) #Get polar
+  scanner <- scanner[between(zenith, zenith.range[1], zenith.range[2]) , 1:2] #Subset zenith rings
 
   ###Extraction of the structure metrics------------------------------
-  #Set the table to return
-  bands.limits <- seq(zenith.range[1], zenith.range[2], zenith.bands-1)
+  #Set the table for results
+
+  bands.limits <- seq(zenith.range[1], zenith.range[2], zenith.bands+1)
 
   bands <- data.table(rings = seq(1, zenith.bands, 1),
                       min.zenith = c(bands.limits[1:length(bands.limits)-1]),
                       max.zenith = c(bands.limits[2:length(bands.limits)]))
 
   frame <- CJ(rings = seq(1, zenith.bands, 1),
-              h = seq(0, max(scan$h), 0.01))
+              h = seq(max(scan$Z), 0, -vertical.range))
   frame$shots <- NA
   frame$returns_below <- NA
   frame <- merge(bands, frame, by = "rings")
 
-  #Set the extraction
+
+  #Set the extraction------------------------------------------------------
 
   if(parallel == FALSE) {
 
     print(paste("", "Estimating the number of returns and shots", sep = ""))  #Progress bar
     pb <- txtProgressBar(min = 1, max = nrow(frame), style = 3)
 
-    results <- foreach(i = 1:nrow(frame), .inorder = FALSE, .combine= rbind, .packages = c("data.table")) %do% {
-
-      setTxtProgressBar(pb, i)
+    results <- foreach(i = 1:nrow(frame), .inorder = TRUE, .combine= rbind, .packages = c("data.table")) %do% {
 
       frame$shots[i] <- nrow(scanner[between(zenith, frame$min.zenith[i], frame$max.zenith[i])])
-      returns <- scan[between(zenith, frame$min.zenith[i], frame$max.zenith[i]) & Z <= frame$h[i]]
+      frame$returns_below[i] <- sum(scan[between(zenith, frame$min.zenith[i], frame$max.zenith[i]) & Z <= frame$h[i], w])
 
-      if(returns == "multiple") {
-        frame$returns_below[i] <- sum(returns$w)
-
-      } else {
-        frame$returns_below[i] <- nrow(returns)
-
-      }
-
-        return(frame[i])
+      return(frame[i])
     }
   }
 
@@ -155,18 +150,10 @@ fstructure <- function(scan, zenith.range, zenith.bands, azimuth.range, TLS.reso
     cl <- makeCluster(cores, outfile="")
     registerDoSNOW(cl)
 
-    results <- foreach(i = 1:nrow(frame), .inorder = FALSE, .combine= rbind, .packages = c("data.table"), .options.snow = opts) %dopar% {
+    results <- foreach(i = 1:nrow(frame), .inorder = TRUE, .combine= rbind, .packages = c("data.table"), .options.snow = opts) %dopar% {
 
       frame$shots[i] <- nrow(scanner[between(zenith, frame$min.zenith[i], frame$max.zenith[i])])
-      returns <- scan[between(zenith, frame$min.zenith[i], frame$max.zenith[i]) & Z <= frame$h[i]]
-
-      if(returns == "multiple") {
-        frame$returns_below[i] <- sum(returns$w)
-
-      } else {
-        frame$returns_below[i] <- nrow(returns)
-
-      }
+      frame$returns_below[i] <- sum(scan[between(zenith, frame$min.zenith[i], frame$max.zenith[i]) & Z <= frame$h[i], w])
 
       return(frame[i])
     }
@@ -176,7 +163,31 @@ fstructure <- function(scan, zenith.range, zenith.bands, azimuth.range, TLS.reso
   }
 
   results[, Pgap := (1- (returns_below/shots)), by = seq_len(nrow(frame))]
-  results[, L := (-1.1*log10(Pgap))]
+
+
+  results <- reshape(results[, c("rings" ,"h", "Pgap")],
+             v.names = "Pgap",
+             idvar = "h",
+             timevar= "rings",
+             direction= "wide")
+
+  col_hinge <- which(abs(x - your.number) == min(abs(x - your.number)))
+
+
+
+
+
+
+
+
+
+
+  for(i in 1:nrow(new)) {
+    ld <- new$L[i+1]-new$L[i]
+    z <- 0.1
+
+    new$f[i] <- ld/z
+  }
 
   return(results)
 }
