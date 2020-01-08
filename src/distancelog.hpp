dfa92c94ae02e1ffaed2467e6be8a44557d8d115
Antguz <antguz06@gmail.com>
2020-01-08 09:38:01 -0700

    Revert "Changes at CEOS"
    This reverts commit 2963e0f10e0f921b499dd41a0417a6895c2752ca.

--- src/distancelog.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef DISTANCELOG_HPP
-#define DISTANCELOG_HPP
-
-#include <Rcpp.h>
-
-Rcpp::IntegerVector distancelog(const NumericMatrix sample, const NumericMatrix base, const NumericVector radius);
-
-#endif
