802ed55e8444d9cea2841beb807eadb7ca6b4b72
Antguz <antguz06@gmail.com>
2020-01-08 18:12:26 -0700

    Fixing error of commits.

--- src/getEigenvalues.cpp
+++ src/getEigenvalues.cpp
@@ -4,7 +4,7 @@
 using namespace Rcpp;
 using namespace arma;
 
-// First it subsamble the matrix to specific rows, then it computes a cov matrix to extract eigenvalues
+// First it subsamble the matrix to specific rows, then it computes a cov matrix to extract eigenvalues.
 
 Rcpp::NumericVector getEigenvalues(Rcpp::NumericMatrix base, Rcpp::NumericVector dis_logical) {
 
