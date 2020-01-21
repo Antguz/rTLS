658d0e14a79a98805445154d5fe35b7a6f1347d8
Antguz <antguz06@gmail.com>
2020-01-21 10:47:13 -0700

    uppdating with arma cube

--- src/dimensionality_rcpp.cpp
+++ src/dimensionality_rcpp.cpp
@@ -14,7 +14,7 @@ using arma::sum;
 using namespace arma;
 
 // [[Rcpp::export]]
-arma::mat dimensionality_rcpp(arma::mat amat, arma::mat bmat, double radius, int threads = 1) {
+arma::cube dimensionality_rcpp(arma::mat amat, arma::mat bmat, arma::vec radius, int threads = 1) {
 
 #ifdef _OPENMP
   if ( threads > 0 ) {
@@ -25,37 +25,53 @@ arma::mat dimensionality_rcpp(arma::mat amat, arma::mat bmat, double radius, int
 
   int an = amat.n_rows;
   int bn = bmat.n_rows;
+  int len_radius = radius.n_elem;
 
-  arma::mat out(an, 4);
+  arma::cube out(an, 4, len_radius);
 
-  Progress p(an, true);
+  Progress p(an*len_radius, true);
 
 #pragma omp parallel for
   for (int i = 0; i < an; i++) {
 
     arma::vec distance(bn);
 
-    if (! Progress::check_abort() ) {
-      p.increment(); // update progress
+    for (int j = 0; j < bn; j++) { //Loop to estimate the distance
 
-      for (int j = 0; j < bn; j++) { //Loop to estimate the distance
-
-        distance(j) = sqrt(pow((bmat(j, 0) - amat(i, 0)), 2.0) + pow((bmat(j, 1) - amat(i, 1)), 2.0) + pow((bmat(j, 2) - amat(i, 2)), 2.0));
-      }
+      distance(j) = sqrt(pow((bmat(j, 0) - amat(i, 0)), 2.0) + pow((bmat(j, 1) - amat(i, 1)), 2.0) + pow((bmat(j, 2) - amat(i, 2)), 2.0));
     }
 
-    arma::mat basemat(bmat.begin(), bn, 3, false);
+    for (int k = 0; k < len_radius; k++) {
 
-    arma::mat basesub = basemat.rows(find(distance > 0 && distance <= radius));
+      if (! Progress::check_abort() ) {
+        p.increment(); // update progress
+      }
 
-    arma::mat covmat =  arma::cov(basesub);
+      arma::mat basemat(bmat.begin(), bn, 3, false);
 
-    arma::vec eigenvalues =  arma::eig_sym(covmat);
+      arma::mat basesub = basemat.rows(find(distance > 0 && distance <= radius[k]));
 
-    out(i , 0) = basesub.n_rows;
-    out(i , 1) = eigenvalues[2]/sum(eigenvalues); //Row index of amat
-    out(i , 2) = eigenvalues[1]/sum(eigenvalues); //Value of column 0 of bmat
-    out(i , 3) = eigenvalues[0]/sum(eigenvalues); //Value of column 1 of bmat
+      if(basesub.n_rows > 2) {
+
+        arma::mat covmat =  arma::cov(basesub);
+
+        arma::vec eigenvalues =  arma::eig_sym(covmat);
+
+        out(i , 0, k) = basesub.n_rows;
+        out(i , 1, k) = eigenvalues[2]/sum(eigenvalues); //Row index of amat
+        out(i , 2, k) = eigenvalues[1]/sum(eigenvalues); //Value of column 0 of bmat
+        out(i , 3, k) = eigenvalues[0]/sum(eigenvalues); //Value of column 1 of bmat
+
+      } else {
+
+        out(i , 0, k) = basesub.n_rows;
+        out(i , 1, k) = 0; //Row index of amat
+        out(i , 2, k) = 0; //Value of column 0 of bmat
+        out(i , 3, k) = 0; //Value of column 1 of bmat
+
+      }
+
+    }
 
   }
 
