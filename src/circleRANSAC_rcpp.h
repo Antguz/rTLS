d938c92d5ec8dcab4f840b0a742c3c5bd880c13e
J. Antonio Guzmán Q <antguz06@gmail.com>
2020-12-22 20:18:16 -0700

    Implementing cicle fitting RASCAN

---
+++ src/circleRANSAC_rcpp.h
@@ -0,0 +1,8 @@
+#ifndef MEANDIS_KNN_H
+#define MEANDIS_KNN_H
+
+#include <RcppArmadillo.h>
+
+arma::mat circleRANSAC_rcpp(arma::mat cloud, double fract_points, double pconf, arma::vec poutlier, int max_iterations, int threads = 1)
+
+#endif
