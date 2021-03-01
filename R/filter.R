8a3252c4583c372580d6d10e77f61fea24ae7993
J. Antonio Guzmán Q <antguz06@gmail.com>
2021-02-28 21:17:04 -0700

    fix documentation filter

--- R/filter.R
+++ R/filter.R
@@ -9,7 +9,7 @@
 #' @param k An \code{integer} vector representing the number of neighbors to consider. This needs be used if \code{method = "SOR"}.
 #' @param nSigma A \code{numeric} vector representing the standard deviation multiplier. This needs to be used if \code{method = "SOR"}.
 #' @param edge_length A positive \code{numeric} vector with the voxel-edge length for the x, y, and z coordinates. This needs to be used if \code{method = "voxel_center"}.
-#' @param build A \codecharacter} describing the search structure to be used: \code{"kdtree"}, \code{"kmeans"}, \code{"linear"}. Default \code{"kdtree"}.
+#' @param build A \code{character} describing the search structure to be used: \code{"kdtree"}, \code{"kmeans"}, \code{"linear"}. Default \code{"kdtree"}.
 #' @param threads An \code{integer} specifying the number of threads to use for parallel processing. Experiment to see what works best for your data on your hardware.
 #' @param checks Number of checks during searching. Higher value gives better search precision but takes longer. \code{checks = 20} as default.
 #'
@@ -44,7 +44,7 @@ filter <- function(cloud, method, radius, min_neighbours, k, nSigma, edge_length
 
   if(method == "SOR") {
 
-    point <- knn(cloud, cloud, (k+1), same = TRUE, build, threads = threads, checks = checks)
+    point <- knn(cloud, cloud, (k+1), same = TRUE, build = build, threads = threads, checks = checks)
     point <- point[, lapply(.SD, mean), by= query]
     max_distance <- mean(point$distance) + sd(point$distance)*nSigma
     logic_sub <- point$distance <= max_distance
@@ -54,7 +54,7 @@ filter <- function(cloud, method, radius, min_neighbours, k, nSigma, edge_length
 
   if(method == "min_neighbors") {
 
-    neighbors <- radius_search(cloud, cloud, radius, max_neighbour = (min_neighbours+1), same = TRUE, build, threads = threads, checks = checks)
+    neighbors <- radius_search(cloud, cloud, radius, max_neighbour = (min_neighbours+1), same = TRUE, build = build, threads = threads, checks = checks)
     count <- neighbors[, .N, by = query]
     count <- count[N >= min_neighbours,]
     results <- cloud[count$query, ]
@@ -63,7 +63,7 @@ filter <- function(cloud, method, radius, min_neighbours, k, nSigma, edge_length
   if(method == "voxel_center") {
 
     center <- voxels(cloud, edge_length = c(edge_length, edge_length, edge_length), obj.voxels = FALSE)
-    point <- knn(center[, 1:3], cloud, k = 1, same = FALSE, build, threads = threads, checks = checks)
+    point <- knn(center[, 1:3], cloud, k = 1, same = FALSE, build = build, threads = threads, checks = checks)
     results <- cloud[point$ref, ]
 
   }
