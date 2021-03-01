660444d668d1ed15b252e84084d781079b8edcc9
J. Antonio Guzmán Q <antguz06@gmail.com>
2021-02-28 20:15:21 -0700

    deleating old geometry features

--- src/dimensionality_knn_rcpp.h
+++
@@ -1,8 +0,0 @@
-#ifndef DIMENSIONALITY_KNN_H
-#define DIMENSIONALITY_KNN_H
-
-#include <RcppArmadillo.h>
-
-arma::cube dimensionality_knn_rcpp(arma::mat amat, arma::mat bmat, arma::vec k, int threads = 1, bool progress = true);
-
-#endif
--- src/nneighbors_sphere_rcpp.h
+++
@@ -1,8 +0,0 @@
-#ifndef NNEIGHBORS_SPHERE_H
-#define NNEIGHBORS_SPHERE_H
-
-#include <RcppArmadillo.h>
-
-arma::vec nneighbors_sphere_rcpp(arma::mat amat, double radius, int threads = 1, bool progress = true);
-
-#endif
