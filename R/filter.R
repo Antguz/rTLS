d84a6d84dfff8ffc545c9df4dc193477ee73883e
J. Antonio Guzmán Q <antguz06@gmail.com>
2021-11-25 17:43:04 -0600

    Modifications to filter

--- R/filter.R
+++ R/filter.R
@@ -9,9 +9,11 @@
 #' @param k An \code{integer} vector representing the number of neighbors to consider. This needs be used if \code{method = "SOR"}.
 #' @param nSigma A \code{numeric} vector representing the standard deviation multiplier. This needs to be used if \code{method = "SOR"}.
 #' @param edge_length A positive \code{numeric} vector with the voxel-edge length for the x, y, and z coordinates. This needs to be used if \code{method = "voxel_center"}.
-#' @param build A \code{character} describing the search structure to be used: \code{"kdtree"}, \code{"kmeans"}, \code{"linear"}. Default \code{"kdtree"}.
+#' @param distance Type of distance to calculate. \code{"euclidean"} as default. Look \code{hnsw_knn} for more options.
 #' @param threads An \code{integer} specifying the number of threads to use for parallel processing. Experiment to see what works best for your data on your hardware.
-#' @param checks Number of checks during searching. Higher value gives better search precision but takes longer. \code{checks = 20} as default.
+#' @param verbose If TRUE, log messages to the console.
+#' @param progress If TRUE, log a progress bar when \code{verbose = TRUE}. Tracking progress could cause a small overhead.
+#' @param ... Arguments passed to \code{hnsw_build} and \code{hnsw_search}.
 #'
 #' @return A \code{data.table} with the filtered points
 #' @author J. Antonio Guzmán Q.
@@ -40,11 +42,11 @@
 #' rgl::points3d(pc_compare, col = "black") #Original
 #'
 #' @export
-filter <- function(cloud, method, radius, min_neighbours, k, nSigma, edge_length, build = "kdtree", threads = 1L, checks = 20) {
+filter <- function(cloud, method, radius, min_neighbours, k, nSigma, edge_length, distance = "euclidean", threads = 1L, verbose = FALSE, progress = FALSE, ...) {
 
   if(method == "SOR") {
 
-    point <- knn(cloud, cloud, (k+1), same = TRUE, build = build, threads = threads, checks = checks)
+    point <- knn(cloud, cloud, (k+1), same = TRUE, distance = distance, verbose = verbose, progress = progress, ...)
     point <- point[, lapply(.SD, mean), by= query]
     max_distance <- mean(point$distance) + sd(point$distance)*nSigma
     logic_sub <- point$distance <= max_distance
@@ -54,7 +56,7 @@ filter <- function(cloud, method, radius, min_neighbours, k, nSigma, edge_length
 
   if(method == "min_neighbors") {
 
-    neighbors <- radius_search(cloud, cloud, radius, max_neighbour = (min_neighbours+1), same = TRUE, build = build, threads = threads, checks = checks)
+    neighbors <- radius_search(cloud, cloud, radius, max_neighbour = (min_neighbours+1), same = TRUE, threads = threads, verbose = verbose, progress = progress, ...)
     count <- neighbors[, .N, by = query]
     count <- count[N >= min_neighbours,]
     results <- cloud[count$query, ]
@@ -63,9 +65,8 @@ filter <- function(cloud, method, radius, min_neighbours, k, nSigma, edge_length
   if(method == "voxel_center") {
 
     center <- voxels(cloud, edge_length = c(edge_length, edge_length, edge_length), obj.voxels = FALSE)
-    point <- knn(center[, 1:3], cloud, k = 1, same = FALSE, build = build, threads = threads, checks = checks)
+    point <- knn(center[, 1:3], cloud, k = 1, same = FALSE, distance = distance, verbose = verbose, progress = progress)
     results <- cloud[point$ref, ]
-
   }
 
   return(results)
