cb75fc09eb2bb68733ad47fe6dc788d418848639
J. Antonio Guzmán Q <antguz06@gmail.com>
2021-11-25 16:01:49 -0600

    knn from RcppHNSW

--- R/knn.R
+++ R/knn.R
@@ -1,24 +1,22 @@
 #' K Nearest Neighbors
 #'
-#' K nearest neighbors based on FLANN C++
+#' Adapted K nearest neighbors based on RcppHNSW
 #'
 #' @param query A \code{data.table} containing the set of query points where each row represent a point and each column a given coordinate.
 #' @param ref A \code{numeric} containing the set of reference points where each row represent a point and each column a given coordinate.
 #' @param k An \code{integer} describing the number of nearest neighbors to search for.
-#' @param same Logic. If \code{TRUE}, it delete neighbors with distance of 0, useful when the k search is based on the same query.
-#' @param build A \code{character} describing the search structure to be used: "kdtree", "kmeans", "linear".
 #' @param threads An \code{integer} specifying the number of threads to use for parallel processing. Experiment to see what works best for your data on your hardware. If 0, then the maximum allowable cores are used.
-#' @param checks Number of checks during searching. Higher value gives better search precision but takes longer. See FLANN C++ manual for more details.
-#'
+#' @param ... Arguments passed to \code{hnsw_build} and \code{hnsw_search}.
 #' @return A \code{data.table} with three columns describing the indices of the query, ref, and k neighbors and the distances.
 #'
 #' @author J. Antonio Guzmán Q.
 #'
 #' @details
-#' This function is based on the FLAAN C++ Library (Muja and Lowe 2009) for a fast
-#' estimation of neighbors points. It is adapted from rflann (Muja, Lowe, Yee, 2018) to
-#' simplify the workflow with TLS data. If you use this function, please consider cite
-#' both documents.
+#' This function is based on hnswlib C++ library (Malkov & Yashunin 2016) and
+#' its bindings for R (RcppHNSW; Melville 2020) for a fast estimation of neighbors
+#' points. It is adapted to simplify the workflow within rTLS.
+#' If you use this function, please consider cite the C++ library and
+#' \code{RcppHNSW} package.
 #'
 #' @references
 #' Muja, M., Lowe, D.G. (2009). Fast approximate nearest neighbors with automatic algorithm configuration. VISAPP (1), 2(331-340), 2.
@@ -33,15 +31,68 @@
 #' data("pc_tree")
 #'
 #' #knn search using k = 3
-#' knn(pc_tree, pc_tree, k = 3, same = TRUE, "kdtree", checks = 10)
+#' knn(pc_tree, pc_tree, k = 3, same = TRUE)
 #'
 #' @export
-knn <- function(query, ref, k, same = FALSE, build = "kdtree", threads = 1L, checks = 1L) {
+knn <- function(query, ref, k, distance = "euclidean", same = FALSE, threads = 1L, progress = FALSE, ...) {
 
-  results <- knn_rcpp(as.matrix(query), as.matrix(ref), k = k, same = same, build = build, threads = threads, checks = checks)
-  results <- as.data.table(results)
-  colnames(results) <- c("query", "ref", "k_index", "distance")
+  #Initinal arguments
+  if(progress == TRUE) {
+    bar <- "bar"
+  } else if(progress == FALSE) {
+    bar <- NULL
+  }
 
+  if(same == TRUE) {
+    k_final = k+1
+  } else if(same == FALSE) {
+    k_final = k
+  }
+
+  #Modifications and estimation using RcppHNSW
+  dist <- match.arg(distance, c("l2", "euclidean", "cosine", "ip"))
+
+  neig <- hnsw_build(X = as.matrix(ref),
+                     distance = dist,
+                     progress = bar,
+                     n_threads = threads,
+                     ...)
+
+  results <- hnsw_search(X = as.matrix(query),
+                         ann = ann,
+                         k = k_final,
+                         progress = bar,
+                         n_threads = threads,
+                         ...)
+
+  #Index
+  index <- cbind(query = 1:nrow(query),
+                 data.table(results$idx))
+  colnames(index) <- c("query", as.character(c(1:k_final)))
+  index_melt <- melt(index,
+                     id.vars = c("query"),
+                     variable.name = "k_index",
+                     value.name = "ref")
+  #Distance
+  dist <- cbind(query = 1:nrow(query),
+                data.table(results$dist))
+  colnames(dist) <- c("query", as.character(c(1:k_final)))
+  dist_melt <- melt(dist,
+                    id.vars = c("query"),
+                    variable.name = "k_index",
+                    value.name = "distance")
+
+  #Merge index and distance
+  results <- merge(index_melt, dist_melt, by = c("query", "k_index"))
+  results <- results[, c(1,3,2,4)]
+  results$k_index <- as.numeric(as.character(results$k_index))
+
+  if(same == TRUE) {
+    results$k_index <- results$k_index - 1
+    results <- subset(results, k_index != 0)
+  }
+
+  #Export
   return(results)
 
 }
