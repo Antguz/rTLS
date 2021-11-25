5dcffcef81ed5036bdf4353bfb01a2225d638dad
J. Antonio Guzmán Q <antguz06@gmail.com>
2021-11-25 16:34:25 -0600

    Bye knn_rcpp and radius_search_rcpp

--- src/knn_rcpp.h
+++
@@ -1,8 +0,0 @@
-#ifndef KNN_H
-#define KNN_H
-
-#include <RcppArmadillo.h>
-
-arma::mat knn_rcpp(arma::mat query, arma::mat ref, int k, bool same, std::string build, int threads, int checks);
-
-#endif
--- src/radius_search_rcpp.h
+++
@@ -1,8 +0,0 @@
-#ifndef RADIUS_SEARCH_H
-#define RADIUS_SEARCH_H
-
-#include <RcppArmadillo.h>
-
-arma::mat radius_search_rcpp(arma::mat query, arma::mat ref, double radius, int max_neighbour, bool same, std::string build, int threads, int checks);
-
-#endif
